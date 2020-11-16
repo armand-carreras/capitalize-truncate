58b51461d4dc0f7bca3f5c1e68539ec68111eb6a
armand-carreras <armand.carreras@student.bit.es>
2020-11-16 12:19:53 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { CapitalizePipe } from './capitalize.pipe';
-import { SmartTruncatePipe } from './smart-truncate.pipe';
+import { AppComponent } from "./app.component";
+import { CapitalizePipe } from "./capitalize.pipe";
+import { SmartTruncatePipe } from "./smart-truncate.pipe";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CapitalizePipe, SmartTruncatePipe ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, CapitalizePipe, SmartTruncatePipe, FilterPipe],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
